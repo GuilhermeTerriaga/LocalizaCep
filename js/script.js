eb4565de4c541cab6a27b7b649983e1263175ab6
guilherme terriaga <guilherme.terriaga@gmail.com>
2020-05-11 21:03:15 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let btnpegar;
+
+function aoCarregarPagina(){
+    btnpegar = document.getElementById("btnpegar");
+    btnpegar.addEventListener('click', requisicaoCep);
+}
+
+function requisicaoCep(){
+    let cep = document.getElementById("txtcep").value;
+    const endpoint = "https://viacep.com.br/ws/"+cep+"/json";
+
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById("lblrua").textContent = data.logradouro;
+        document.getElementById("lblbairro").textContent = data.bairro;
+        document.getElementById("lblcidade").textContent = data.localidade;
+        
+        
+    })
+    .catch(() => console.log("Opa! deu ruim aí!"));
+}
